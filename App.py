deb23ef585b525297973e377bc6ec207ec61574f
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-10-03 10:18:52 -0500

    Update App.py

--- App.py
+++ App.py
@@ -1,27 +1,5 @@
-import tensorflow as tf
-from PIL import Image, ImageOps
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-
-
-#Función de predicción
-def predictDigit(image):
-    model = tf.keras.models.load_model("model/handwritten.h5")
-    image = ImageOps.grayscale(image)
-    img = image.resize((28,28))
-    img = np.array(img, dtype='float32')
-    img = img/255
-    plt.imshow(img)
-    plt.show()
-    img = img.reshape((1,28,28,1))
-    pred= model.predict(img)
-    result = np.argmax(pred[0])
-    return result
-
-
 
 st.title("My first canvas")
 st.write("Draw anything you want!")
@@ -54,13 +32,5 @@ canvas_result = st_canvas(
     drawing_mode = drawing_mode
 )
 
-if st.button('Predecir'):
-    if canvas_result.image_data is not None:
-        input_numpy_array = np.array(canvas_result.image_data)
-        input_image = Image.fromarray(input_numpy_array.astype('uint8'),'RGBA')
-        input_image.save('prediction/img.png')
-        img = Image.open("prediction/img.png")
-        res = predictDigit(img)
-        st.header('El Digito es : ' + str(res))
-    else:
-        st.header('Por favor dibuja en el canvas el digito.')
+if image_data is not None:
+    st.image(image_data)
