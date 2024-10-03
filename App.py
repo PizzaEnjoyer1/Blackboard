94d83d80c45465f16c0c649184fd5dc730ba7e61
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-10-03 10:22:21 -0500

    Update App.py

--- App.py
+++ App.py
@@ -17,14 +17,12 @@ with st.sidebar:
   stroke_width = st.slider("Grosor del pincel", 1, 100, 10)
 
   stroke_color = st.color_picker("Selecciona un color", "#000000")
-
-  fill_color = st.color_picker("Selecciona el color de relleno, "#FFFFFF")
   
   bg_color = st.color_picker("Enter background color", "#FFFFFF")
 
  
 canvas_result = st_canvas(
-    fill_color=fill_color  # Fixed fill color with some opacity
+    fill_color="rgba(255, 165, 0, 0.3)"
     stroke_width=stroke_width,
     stroke_color=stroke_color,
     background_color=bg_color,
